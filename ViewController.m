91b43083d2ae347722122e7627799d466031f1e2
夕若 <mr_xi@foxmail.com>
2015-08-07 17:24:44 +0800

    创建Demo

---
+++ ViewController.m
@@ -0,0 +1,71 @@
+//
+//  ViewController.m
+//  XRPopMenuDemo
+//
+//  Created by XiRuo on 15/8/7.
+//  Copyright (c) 2015年 Xiruo. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XRPopMenuView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self configButton];
+}
+
+- (void)configButton
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setFrame:CGRectMake(.0f, .0f, 80.0f, 40.0f)];
+    [button setTitle:@"点击弹出" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    button.center = self.view.center;
+    [self.view addSubview:button];
+    [button addTarget:self action:@selector(buttonClick:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)buttonClick:(id)sender
+{
+    [self showPopMenu];
+}
+
+- (void)showPopMenu
+{
+    XRPopMenuView *menuView = [[XRPopMenuView alloc] init];
+    [menuView addMenuItemWithTitle:@"创建标签" andIcon:[UIImage imageNamed:@"editButton"] andSelectedBlock:^{
+        NSLog(@"创建标签 selected");
+    }];
+    [menuView addMenuItemWithTitle:@"添加标签" andIcon:[UIImage imageNamed:@"markButton"] andSelectedBlock:^{
+        NSLog(@"添加标签 selected");
+    }];
+    [menuView addMenuItemWithTitle:@"创建标签" andIcon:[UIImage imageNamed:@"editButton"] andSelectedBlock:^{
+        NSLog(@"创建标签 selected");
+    }];
+    [menuView addMenuItemWithTitle:@"添加标签" andIcon:[UIImage imageNamed:@"markButton"] andSelectedBlock:^{
+        NSLog(@"添加标签 selected");
+    }];
+    [menuView addMenuItemWithTitle:@"创建标签" andIcon:[UIImage imageNamed:@"editButton"] andSelectedBlock:^{
+        NSLog(@"创建标签 selected");
+    }];
+    [menuView addMenuItemWithTitle:@"添加标签" andIcon:[UIImage imageNamed:@"markButton"] andSelectedBlock:^{
+        NSLog(@"添加标签 selected");
+    }];
+
+    
+    [menuView show];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
